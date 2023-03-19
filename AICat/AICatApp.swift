42e981f0ebad060e2dc4c1b7f645b4c4571bc4e7
Lei Pan <panlei106@gmail.com>
2023-03-19 14:22:43 +0800

    Initial Commit

---
+++ AICat/AICatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICatApp.swift
+//  AICat
+//
+//  Created by Lei Pan on 2023/3/19.
+//
+
+import SwiftUI
+
+@main
+struct AICatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
