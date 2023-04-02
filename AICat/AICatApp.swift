d05ebde5345fb8113b8d9ceb4323adc293c63d4a
Lei Pan <panlei106@gmail.com>
2023-04-02 11:49:07 +0800

    feat: add appcenter sdk

--- AICat/AICatApp.swift
+++ AICat/AICatApp.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 import Blackbird
 import Foundation
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
 
 @main
 struct AICatApp: App {
+    init() {
+        AppCenter.start(
+            withAppSecret: appCenterSecretKey,
+            services: [
+                Analytics.self,
+                Crashes.self
+            ]
+        )
+    }
+
     var body: some Scene {
         WindowGroup {
             MainView()
